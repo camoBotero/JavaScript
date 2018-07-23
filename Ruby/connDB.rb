c213050b6b56b3374e6852c2e66e87afeb0807cf
camoBotero <41131276+camoBotero@users.noreply.github.com>
2018-07-22 22:49:53 -0500

    Add files via upload

--- Ruby/connDB.rb
+++ Ruby/connDB.rb
@@ -1,10 +1,6 @@
 require 'pg'
-con = PG::Connection.new("localhost","5432","","","fullstack","postgres","admin")
-puts con
 
-var = con.exec("Select * from personas")
-#ins = con.exec("INSERT INTO personas (nombre, apellido, cedula, telefono, estado) VALUES ('Diana','Sierra','4789',11111, true)")
-puts var
+con = PG::Connection.new("localhost","5432","","","fullstack","postgres","admin")
 
 class Crud
 	require 'pg'
@@ -12,30 +8,37 @@ class Crud
 	def create(nombre, apellido, cedula, telefono, estado)
 		con = PG::Connection.new("localhost","5432","","","fullstack","postgres","admin")
 		sqlInsert = "INSERT INTO personas (nombre, apellido, cedula, telefono, estado) 
-			VALUES ('" +nombre+"', '"+apellido+"', "+cedula+", "+telefono.to_s+", "+estado.to_s+")"
-		# sqlInsert = "INSERT INTO personas (nombre, apellido, cedula, telefono, estado) 
-		# VALUES ('Sebas','Botero','11',123,true)"
-		#puts sqlInsert
+			VALUES ('" + nombre + "' 
+			        , '" + apellido + "'
+			        , '" + cedula + "'
+			        , '" + telefono.to_s + "'
+			        , '" + estado.to_s + "')"		
 		return con.exec(sqlInsert);
-	end
+	end	
 	def read
+		con = PG::Connection.new("localhost","5432","","","fullstack","postgres","admin")
 		con.exec("SELECT * FROM personas")
 	end
 	def update(id_persona, nombre, apellido, cedula, telefono, estado)
 		con = PG::Connection.new("localhost","5432","","","fullstack","postgres","admin")
-		sqlUpdate = "UPDATE personas SET nombre = '"+nombre+"', apellido = '"+apellido+"', cedula = "+cedula+", telefono = "+telefono.to_s+
-			", estado = "+estado.to_s+" WHERE id_persona = " + id_persona
+		sqlUpdate = "UPDATE personas SET nombre = '" + nombre + 
+		            "' , apellido = '" + apellido + 
+		            "' , cedula = '" + cedula +
+		            "' , telefono = '" + telefono.to_s +
+		            "' , estado = '" + estado.to_s + 
+		            "' WHERE id_persona = " + id_persona.to_s
+
 		return con.exec(sqlUpdate);
 	end
 	def delete(id_persona)
 		con = PG::Connection.new("localhost","5432","","","fullstack","postgres","admin")
-		sqlDelete = "DELETE FROM personas WHERE id_persona = " + id_persona
+		sqlDelete = "DELETE FROM personas WHERE id_persona = " + id_persona.to_s
 		return con.exec(sqlDelete)
 	end
 end
 
 crud = Crud.new
-#crud.create('Sebas','Botero','7898',77777,true)
-crud.update(5, 'Sebastian','Botero','7898',77777,true)
-#crud.create('Sebas','Botero','7898',77777,true)
-#crud.create('Sebas','Botero','7898',77777,true)
+#crud.create('David','Ospina','1111',684258,true)
+#crud.update(3, 'Sebastian','Botero','9874',1278,false)
+#crud.read()
+crud.delete(4)
